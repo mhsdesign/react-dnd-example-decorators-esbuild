185d579489732310684ab4a288eb82a025528cd5
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-10-17 23:35:06 +0200

    Pin react and react-dnd deps

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ require("esbuild").build({
   entryPoints: ["src/index.js", "src/index.html"],
   outdir: "dist",
   bundle: true,
+  logLevel: "info",
   loader: {
     ".html": "copy",
     ".js": "jsx"
